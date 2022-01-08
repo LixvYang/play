10bd23501bf98d3ede443503f0ca2153332c90df
llll0 <a18331685491@163.com>
2022-01-08 10:26:38 +0800

    first
    first

---
+++ play.ts
@@ -0,0 +1,77 @@
+
+
+
+
+//% weight=0 color=#0fbc11  icon="\uf207" block="play1"
+namespace play1 {
+
+
+    /**
+    * PLAY: full speed move right
+    */
+    //% weight=9
+    //% blockId=play_right block="right"
+    export function right () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P3, 512)
+    pins.analogWritePin(AnalogPin.P4, 512)
+}
+
+
+    /**
+    * PLAY: full speed move back
+    */
+    //% weight=8
+    //% blockId=play_back block="back"
+    export function back () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P3, 512)
+    pins.analogWritePin(AnalogPin.P4, 512)
+}
+
+
+
+    /**
+    * PLAY: full speed turn left
+    */
+    //% weight=7
+    //% blockId=play_left block="left"
+    export function left () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P3, 512)
+    pins.analogWritePin(AnalogPin.P4, 512)
+}
+
+
+    /**
+    * PLAY: full speed move right
+    */
+    //% weight=6
+    //% blockId=play_right block="right"
+    export function right () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P3, 512)
+    pins.analogWritePin(AnalogPin.P4, 512)
+}
+
+
+    /**
+    * PLAY: stop
+    */
+    //% weight=5
+    //% blockId=play_stop block="stop"
+    export function stop () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P3, 1023)
+    pins.analogWritePin(AnalogPin.P4, 1023)
+}
+
+
+
+}
+
